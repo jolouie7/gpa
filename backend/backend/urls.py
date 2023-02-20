19b25a0a95f8c78ab67b659a3b02e7a3e426426f
Joseph <joseph161216@gmail.com>
2023-02-20 14:47:38 -0800

    Add basic FE

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
